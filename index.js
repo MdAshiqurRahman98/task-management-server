3da7914b9ec9e58ba3b86b613357c8d08133f110
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-12-21 00:58:57 +0600

    custom middlewares added

--- index.js
+++ index.js
@@ -19,22 +19,47 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // MongoDB connection
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
+// Custom middlewares
+const logger = async (req, res, next) => {
+    console.log('called: ', req.hostname, req.originalUrl);
+    console.log('log: info', req.method, req.url);
+    next();
+}
+
+const verifyToken = async (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('Token in the middleware: ', token);
+
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' });
+        }
+
+        req.user = decoded;
+        next();
+    })
+}
+
 async function run() {
-  try {
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
