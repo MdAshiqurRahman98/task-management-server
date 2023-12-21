bcf361983a1a6021107c6b3a8d3d6801d1e21ddc
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-12-22 01:16:51 +0600

    auth related API added

--- index.js
+++ index.js
@@ -26,33 +26,70 @@ const client = new MongoClient(uri, {
     }
 });
 
-// Custom middlewares
-const logger = async (req, res, next) => {
-    console.log('called: ', req.hostname, req.originalUrl);
-    console.log('log: info', req.method, req.url);
-    next();
-}
-
-const verifyToken = async (req, res, next) => {
-    const token = req?.cookies?.token;
-    // console.log('Token in the middleware: ', token);
-
-    if (!token) {
-        return res.status(401).send({ message: 'unauthorized access' });
-    }
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({ message: 'unauthorized access' });
-        }
-
-        req.user = decoded;
-        next();
-    })
-}
-
 async function run() {
     try {
+        const taskCollection = client.db("taskDB").collection("tasks");
+
+        // Custom middlewares
+        const logger = async (req, res, next) => {
+            console.log('called: ', req.hostname, req.originalUrl);
+            console.log('log: info', req.method, req.url);
+            next();
+        }
+
+        const verifyToken = async (req, res, next) => {
+            const token = req?.cookies?.token;
+            // console.log('Token in the middleware: ', token);
+
+            if (!token) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' });
+                }
+
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        // Auth related APIs
+        try {
+            app.post('/jwt', logger, async (req, res) => {
+                const user = req.body;
+                console.log('User: ', user);
+
+                const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                    expiresIn: '1h'
+                });
+
+                res
+                    .cookie('token', token, {
+                        httpOnly: true,
+                        secure: true,
+                        sameSite: 'none',
+                        maxAge: 24 * 60 * 60 * 1000   // 24 hours
+                    })
+                    .send({ success: true });
+            })
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+        try {
+            app.post('/logout', async (req, res) => {
+                const user = req.body;
+                console.log('Logging out', user);
+                res.clearCookie('token', { maxAge: 0 }).send({ success: true });
+            })
+        }
+        catch (error) {
+            console.log(error);
+        }
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
