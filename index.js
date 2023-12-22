8336ba2da7092d09ed4cfaae81bcbded98b19cbc
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-12-22 19:49:40 +0600

    production server link updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['https://taskflowweb.netlify.app'],
+    origin: ['https://joyful-lokum-eabe50.netlify.app'],
     credentials: true
 }));
 app.use(express.json());
